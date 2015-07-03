1567ba5cb38b1f2321efd59fdb367b4e61436e7c
Isha <isha@Ishas-MacBook-Air.local>
2015-07-03 13:27:18 -0400

    reservation model

--- SeatYourself/db/schema.rb
+++ SeatYourself/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702213428) do
+ActiveRecord::Schema.define(version: 20150702214839) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "date_time"
+    t.integer  "party_size"
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "firstname"
