e114c0aeee0103be08af497d64b13e9c171d65f7
Isha <isha@Ishas-MacBook-Air.local>
2015-07-03 15:07:40 -0400

    WORKING sign up page

--- SeatYourself/config/routes.rb
+++ SeatYourself/config/routes.rb
@@ -1,9 +1,31 @@
 Rails.application.routes.draw do
+  
+  root 'welcome#index'
+
+  # get 'users/index'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
+  # get 'users/show'
+
+  # get 'users/edit'
+
+  # get 'users/update'
+
+  # get 'users/destroy'
+
+  resources :users  
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
